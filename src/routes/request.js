00cd11b43f94e179b2e2f35010a7673c0aec645c
preetsinghwal <preetsinghwal38@gmail.com>
2026-02-19 20:54:18 +0530

    DEVTINDER: added connection API, and some refactoring

--- src/routes/request.js
+++ src/routes/request.js
@@ -15,7 +15,7 @@ requestRouter.post('/request/send/:status/:toUserId', userAuth, async (req, res)
             return res
             .status(404)
             .json({
-                message: 'Invalid status type' + status
+                message: 'Invalid status type ' + status
             })
         }
 
@@ -72,7 +72,7 @@ requestRouter.post('/request/review/:status/:requestId', userAuth, async(req,res
     try {
         const {status, requestId} = req.params;
         const loggedInUser = req.user;
-        const allowedStatus = ['acccepted', 'rejected'];
+        const allowedStatus = ['accepted', 'rejected'];
 
         if(!allowedStatus.includes(status)) {
             return res.status(400).json({message: 'Status not allowed'});
@@ -85,7 +85,7 @@ requestRouter.post('/request/review/:status/:requestId', userAuth, async(req,res
         });
 
         if(!connectionRequest) {
-            return res.status(400).json({message: 'Connection Request not Found'});
+            return res.status(404).json({message: 'Connection Request not Found'});
         }
 
         connectionRequest.status = status;
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,8 +3,10 @@ const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
 const userRouter = express.Router();
 
+const USER_SAFE_DATA = ['firstName', 'lastName', 'photoUrl', 'age', 'gender', 'about', 'skills']
 
-userRouter.get('/user/request/received', userAuth, async (req,res)=> {
+
+userRouter.get('/user/requests/received', userAuth, async (req,res)=> {
     try{
         const loggedInUser = req.user;
         
@@ -13,13 +15,41 @@ userRouter.get('/user/request/received', userAuth, async (req,res)=> {
             status: 'interested'
         }).populate(
             "fromUserId",
-            ['firstName', 'lastName', 'photoUrl', 'age', 'gender', 'about', 'skills']
+            USER_SAFE_DATA
         )
 
         res.status(200).json({
             message: 'Data Fetched Successfully',
             data: connectionRequests
         })
+    }catch(err) {
+        res.status(400).send('ERROR: ' + err.message);
+    }
+})
+
+userRouter.get('/user/connections', userAuth, async (req,res)=> {
+    try {
+
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {toUserId: loggedInUser._id, status: 'accepted'},
+                {toUserId: loggedInUser._id, status: 'accepted'}
+            ]
+        })
+        .populate('fromUserId', USER_SAFE_DATA)
+        .populate('toUserId', USER_SAFE_DATA)
+
+        const data = connectionRequests.map((row)=> {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.status(200).json({data})
+
+
     }catch(err) {
         res.status(400).send('ERROR: ' + err.message);
     }
