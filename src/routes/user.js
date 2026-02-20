ae82de770ae0bdea5a0993686f9223a0aa221a07
preetsinghwal <preetsinghwal38@gmail.com>
2026-02-20 12:10:45 +0530

    DEVTINDER: added pagination in /feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -59,6 +59,12 @@ userRouter.get('/user/connections', userAuth, async (req,res)=> {
 userRouter.get('/feed', userAuth, async (req,res)=> {
     try {
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        
+        const skip = (page - 1) * limit;
+
         const loggedInUser = req.user;
         const connectionRequest = await ConnectionRequest.find({
             $or: [
@@ -79,7 +85,7 @@ userRouter.get('/feed', userAuth, async (req,res)=> {
                 {_id: {$nin: Array.from(hideUsersFromFeed)}},
                 {_id: {$ne: loggedInUser._id}}
             ]
-        }).select(USER_SAFE_DATA)
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit)
 
         res.status(200).send(users);
 
