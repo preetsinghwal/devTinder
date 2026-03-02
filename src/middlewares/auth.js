242b19ad6968899de569b18901e99cbc433bea39
preetsinghwal <preetsinghwal38@gmail.com>
2026-03-02 15:49:47 +0530

    DEVTINDER: added valid error in userAuth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
     try {
         const { token } = req.cookies;
         if (!token) {
-            throw new Error('Token is not Valid!');
+            return res.status(401).send('Please Login')
         }
 
         const decodedObj = await jwt.verify(token, 'DEV@TinderV1.0');
